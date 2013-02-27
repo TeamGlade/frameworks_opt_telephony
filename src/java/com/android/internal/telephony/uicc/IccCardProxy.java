6a0a000a3ce0401d28915d4a924539d0ab211e97
Alan Wang <flwang@codeaurora.org>
2013-02-27 09:41:02 -0800

    Telephony: Don't read records from card if not needed.
    Specifically handle the scenario where there is an RUIM card
     inserted but we are in NV mode. In this case, don't read
    records from the card.
    IccCardProxy tells the IccRecord that it will be requiring records
    from the card. RuimRecord waits for this indication before fetching
    records from the card
    
    Change-Id: Ifa95dda2958922901e887b386c55556f947a881d
    
    Conflicts:
    	src/java/com/android/internal/telephony/uicc/RuimRecords.java

--- src/java/com/android/internal/telephony/uicc/IccCardProxy.java
+++ src/java/com/android/internal/telephony/uicc/IccCardProxy.java
@@ -49,6 +49,7 @@ import com.android.internal.telephony.uicc.IccCardApplicationStatus.PersoSubStat
 import com.android.internal.telephony.uicc.IccCardStatus.CardState;
 import com.android.internal.telephony.uicc.IccCardStatus.PinState;
 import com.android.internal.telephony.uicc.UiccController;
+import com.android.internal.telephony.uicc.RuimRecords;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -161,6 +162,32 @@ public class IccCardProxy extends Handler implements IccCard {
                 mCurrentAppType = UiccController.APP_FAM_3GPP2;
             }
             updateQuietMode();
+            updateActiveRecord();
+        }
+    }
+
+    /**
+     * This method sets the IccRecord, corresponding to the currently active
+     * subscription, as the active record.
+     */
+    private void updateActiveRecord() {
+        log("updateActiveRecord app type = " + mCurrentAppType +
+                "mIccRecords = " + mIccRecords);
+
+        if (mIccRecords == null) {
+            return;
+        }
+
+        if (mCurrentAppType == UiccController.APP_FAM_3GPP2) {
+            int newSubscriptionSource = mCdmaSSM.getCdmaSubscriptionSource();
+            if (newSubscriptionSource == CdmaSubscriptionSourceManager.SUBSCRIPTION_FROM_RUIM) {
+                // Set this as the Active record.
+                log("Setting Ruim Record as active");
+                mIccRecords.recordsRequired();
+            }
+        } else if (mCurrentAppType == UiccController.APP_FAM_3GPP) {
+            log("Setting SIM Record as active");
+            mIccRecords.recordsRequired();
         }
     }
 
@@ -289,6 +316,7 @@ public class IccCardProxy extends Handler implements IccCard {
                 break;
             case EVENT_CDMA_SUBSCRIPTION_SOURCE_CHANGED:
                 updateQuietMode();
+                updateActiveRecord();
                 break;
             case EVENT_SUBSCRIPTION_ACTIVATED:
                 log("EVENT_SUBSCRIPTION_ACTIVATED");
@@ -361,6 +389,7 @@ public class IccCardProxy extends Handler implements IccCard {
                 mUiccApplication = newApp;
                 mIccRecords = newRecords;
                 registerUiccCardEvents();
+                updateActiveRecord();
             }
 
             updateExternalState();
