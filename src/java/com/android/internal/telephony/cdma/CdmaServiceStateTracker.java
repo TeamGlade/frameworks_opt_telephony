51ef9ed9e3a7733d1929e3d79b189de4172b3f65
Manish Kumar <manishku@codeaurora.org>
2014-04-03 22:03:52 +0800

    CdmaSST: trigger getCDMASubscription after EVENT_RUIM_RECORDS_LOADED.
    1. In CdmaLteSST, when EVENT_RUIM_RECORDS_LOADED, OTASP state
    (MDN, MIN, SID, NID, PRL-Ver) will be loaded from RUIM card, and
    ATEL won't trigger getCDMASubscription to get OTASP state from
    network again. But actually user need OTASP state from network
    instead of from card.
    
    2. Move EVENT_RUIM_RECORDS_LOADED implementation from CdmaLteSST
    to parent class CdmaSST. Because this situation is also suitable
    for CdmaSST.
    
    3. Remove LTE_ON_CDMA check in EVENT_RUIM_READY, and get rid of
    getCDMASubscription, which will be done in EVENT_RUIM_RECORDS_LOADED.
    
    CRs-Fixed: 634984
    
    Conflicts:
    src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
    
    Conflicts:
    src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
    Change-Id: I5d211d3a35969686199c90b262a4cc90bf0c9eef
    
    Conflicts:
    	src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java

--- src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
+++ src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
@@ -60,6 +60,7 @@ import com.android.internal.telephony.dataconnection.DcTrackerBase;
 import com.android.internal.telephony.uicc.UiccCardApplication;
 import com.android.internal.telephony.uicc.UiccController;
 import com.android.internal.telephony.HbpcdUtils;
+import com.android.internal.telephony.uicc.RuimRecords;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -293,14 +294,8 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
             break;
 
         case EVENT_RUIM_READY:
-            if (mPhone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) {
-                // Subscription will be read from SIM I/O
-                if (DBG) log("Receive EVENT_RUIM_READY");
-                pollState();
-            } else {
-                if (DBG) log("Receive EVENT_RUIM_READY and Send Request getCDMASubscription.");
-                getSubscriptionInfoAndStartPollingThreads();
-            }
+            if (DBG) log("Receive EVENT_RUIM_READY");
+            pollState();
 
             // Only support automatic selection mode in CDMA.
             mCi.getNetworkSelectionMode(obtainMessage(EVENT_POLL_STATE_NETWORK_SELECTION_MODE));
@@ -477,7 +472,19 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
         case EVENT_RUIM_RECORDS_LOADED:
             log("EVENT_RUIM_RECORDS_LOADED: what=" + msg.what);
             updatePhoneObject();
-            updateSpnDisplay();
+            RuimRecords ruim = (RuimRecords)mIccRecords;
+            if ((ruim != null) && ruim.isProvisioned()) {
+                mMdn = ruim.getMdn();
+                mMin = ruim.getMin();
+                parseSidNid(ruim.getSid(), ruim.getNid());
+                mPrlVersion = ruim.getPrlVersion();
+                mIsMinInfoReady = true;
+                updateOtaspState();
+            }
+            // SID/NID/PRL is loaded. Poll service state
+            // again to update to the roaming state with
+            // the latest variables.
+            getSubscriptionInfoAndStartPollingThreads();
             break;
 
         case EVENT_LOCATION_UPDATES_ENABLED:
